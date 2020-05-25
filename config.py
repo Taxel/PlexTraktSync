c02f6e8cb2884f010005bcd4c6fb8e45d0eed9d6
twolaw <twolaw@free.fr>
2020-05-25 15:10:57 +0200

    full path for config.json opening

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import json
+from os import path
 
-with open('config.json', 'r') as fp:
+config_file = path.join(path.dirname(path.abspath(__file__)), "config.json")
+with open(config_file, 'r') as fp:
     CONFIG = json.load(fp)
