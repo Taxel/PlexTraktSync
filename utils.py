f042f8546f4d10ccbe14357c35d3505607532067
Elan Ruusamäe <glen@pld-linux.org>
2020-07-25 20:48:35 +0300

    Add helper methods: load_json, input_text, input_hidden

--- utils.py
+++ utils.py
@@ -1,5 +1,22 @@
 # helper functions
 
+import getpass
+import json
+
+def input_text(msg, default):
+    answer = input('{0} ({1}): '.format(msg, default))
+
+    if answer == '':
+        print(default)
+        return default
+
+    return answer
+
+def input_hidden(msg):
+    reply = getpass.getpass(prompt = msg)
+
+    return reply
+
 def input_yesno(msg):
     answer = str(input(msg + ' (y/n): ')).lower()
     if answer in ['yes', 'y']:
@@ -9,3 +26,8 @@ def input_yesno(msg):
     else:
         print('Invalid input. Try again.')
         return input_yesno(msg)
+
+def load_json(path):
+    with open(path) as f:
+        data = json.load(f)
+    return data
