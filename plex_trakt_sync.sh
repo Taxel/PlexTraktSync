283a9efe751def6d042717405aefe6175971f52e
Elan Ruusamäe <glen@pld-linux.org>
2020-08-23 11:23:26 +0300

    Update run script to rotate logs

--- plex_trakt_sync.sh
+++ plex_trakt_sync.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
-python3 ./main.py
+set -eu
+
+file=$(readlink -f "$0")
+dir=$(dirname "$file")
+date=$(date --rfc-3339=ns | tr ' ' '_')
+
+if python3 "$dir/main.py" "$@"; then
+	mv last_update.log "update-$date.log"
+else
+	mv last_update.log "error-$date.log"
+fi
