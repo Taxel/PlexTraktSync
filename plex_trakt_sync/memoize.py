ff2ba0501db625e61863c5af055165ae5d5d796f
Elan Ruusamäe <glen@pld-linux.org>
2021-03-06 13:34:40 +0200

    Add class for memoize decorator
    https://www.python-course.eu/python3_memoization.php

---
+++ plex_trakt_sync/memoize.py
@@ -0,0 +1,10 @@
+class Memoize:
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = {}
+
+    def __call__(self, *args):
+        if args not in self.memo:
+            self.memo[args] = self.fn(*args)
+
+        return self.memo[args]
