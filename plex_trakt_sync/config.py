1dbfd208eae53258384ad93064057c6b9746063b
Elan Ruusamäe <glen@pld-linux.org>
2021-03-06 10:21:37 +0200

    Load also environment variables to CONFIG
    This adds common interface to load config regardless how the config
    value is loaded

--- plex_trakt_sync/config.py
+++ plex_trakt_sync/config.py
@@ -1,4 +1,6 @@
 import json
+from dotenv import load_dotenv
+from os import getenv
 from plex_trakt_sync.path import config_file
 
 
@@ -14,6 +16,20 @@ class Config(dict):
         with open(config_file, "r") as fp:
             config = json.load(fp)
             self.update(config)
+
+        load_dotenv()
+        if not getenv("PLEX_TOKEN") or not getenv("TRAKT_USERNAME"):
+            print("First run, please follow those configuration instructions.")
+            from get_env_data import get_env_data
+            get_env_data()
+            load_dotenv()
+
+        self["PLEX_BASEURL"] = getenv("PLEX_BASEURL")
+        self["PLEX_FALLBACKURL"] = getenv("PLEX_FALLBACKURL")
+        self["PLEX_TOKEN"] = getenv("PLEX_TOKEN")
+        self["PLEX_USERNAME"] = getenv("PLEX_USERNAME")
+        self["TRAKT_USERNAME"] = getenv("TRAKT_USERNAME")
+
         self.initialized = True
 
 
