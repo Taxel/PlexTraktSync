05e2205f63eb05a30ac36128da3f53ed1cf8138a
Elan Ruusamäe <glen@pld-linux.org>
2021-03-06 10:06:49 +0200

    Update config module to lazy initialize
    This will load config on first access, not at module import time

--- plex_trakt_sync/config.py
+++ plex_trakt_sync/config.py
@@ -1,5 +1,20 @@
 import json
 from plex_trakt_sync.path import config_file
 
-with open(config_file, 'r') as fp:
-    CONFIG = json.load(fp)
+
+class Config(dict):
+    initialized = False
+
+    def __getitem__(self, item):
+        if not self.initialized:
+            self.initialize()
+        return dict.__getitem__(self, item)
+
+    def initialize(self):
+        with open(config_file, "r") as fp:
+            config = json.load(fp)
+            self.update(config)
+        self.initialized = True
+
+
+CONFIG = Config()
