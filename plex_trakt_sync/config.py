8cb8195af7216152be92d840885dfc12613ddc50
Elan Ruusamäe <glen@pld-linux.org>
2021-03-19 21:26:47 +0200

    Add save() method to Config class

--- plex_trakt_sync/config.py
+++ plex_trakt_sync/config.py
@@ -1,10 +1,18 @@
 import json
 from dotenv import load_dotenv
 from os import getenv
-from plex_trakt_sync.path import config_file
+from plex_trakt_sync.path import config_file, env_file
 
 
 class Config(dict):
+    env_keys = [
+        "PLEX_BASEURL",
+        "PLEX_FALLBACKURL",
+        "PLEX_TOKEN",
+        "PLEX_USERNAME",
+        "TRAKT_USERNAME",
+    ]
+
     initialized = False
 
     def __getitem__(self, item):
@@ -31,6 +39,12 @@ class Config(dict):
         self["TRAKT_USERNAME"] = getenv("TRAKT_USERNAME")
 
         self.initialized = True
+
+    def save(self):
+        with open(env_file, "w") as txt:
+            txt.write("# This is .env file for PlexTraktSync\n")
+            for key in self.env_keys:
+                txt.write("{}={}\n".format(key, self[key]))
 
 
 CONFIG = Config()
