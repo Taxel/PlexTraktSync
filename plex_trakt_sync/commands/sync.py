6126319154b84af265a566f5f9500b770035a370
Elan Ruusamäe <glen@pld-linux.org>
2021-04-08 00:23:14 +0300

    Drop unused process_show_section from main.py
    drop now empty main.py

--- plex_trakt_sync/commands/sync.py
+++ plex_trakt_sync/commands/sync.py
@@ -4,7 +4,6 @@ from plex_trakt_sync.requests_cache import requests_cache
 from plex_trakt_sync.plex_server import get_plex_server
 from plex_trakt_sync.config import CONFIG
 from plex_trakt_sync.decorators import measure_time
-from plex_trakt_sync.main import process_show_section
 from plex_trakt_sync.plex_api import PlexApi, PlexLibraryItem
 from plex_trakt_sync.trakt_api import TraktApi
 from plex_trakt_sync.trakt_list_util import TraktListUtil
