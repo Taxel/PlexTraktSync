eb637966a37dcbb8197f689c8b3f5f960650d3b6
Elan Ruusamäe <glen@pld-linux.org>
2021-04-10 15:24:00 +0300

    Define Dolby Vision 4K w/ E-AC3/DDP+ + Dolby Atmos audio test input

--- tests/test_collection_metadata.py
+++ tests/test_collection_metadata.py
@@ -22,6 +22,38 @@ testdata = [
             'audio_channels': '2.0',
         }
     ),
+    (
+        make(
+            addedAt=datetime(1999, 1, 1),
+            media=[
+                make(
+                    videoResolution="720",
+                    audioChannels=2,
+                    parts=make(
+                        streams=[
+                            make(
+                                streamType="1",
+                                codec="hevc",
+                                displayTitle="4K (HEVC Main 10)",
+                            ),
+                            make(
+                                streamType="2",
+                                channels="6",
+                                audioChannelLayout="5.1(side)",
+                                displayTitle="English (EAC3 5.1)",
+                            ),
+                        ]
+                    )
+                ),
+            ],
+        ),
+        {
+            'collected_at': '1998-12-31:T22:00:00.000Z',
+            'media_type': 'digital',
+            'resolution': 'hd_720p',
+            'audio_channels': '2.0',
+        }
+    ),
 ]
 
 
