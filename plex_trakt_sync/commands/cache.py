254ef2e0bc8dc2ed4c866c9238b0496083fcf613
Elan Ruusamäe <glen@pld-linux.org>
2021-04-10 21:52:15 +0300

    Print urls in cache with cache command

--- plex_trakt_sync/commands/cache.py
+++ plex_trakt_sync/commands/cache.py
@@ -1,8 +1,17 @@
 import click
+from requests_cache import CachedSession
+
+from plex_trakt_sync.path import trakt_cache
+
 
 @click.command()
 def cache():
     """
     Manage and analyze Requests Cache.
     """
-    pass
+    session = CachedSession(cache_name=trakt_cache, backend='sqlite')
+    click.echo(f"Cache status:\n{session.cache}\n")
+
+    click.echo(f"URLs:")
+    for url in session.cache.urls:
+        click.echo(f"- {url}")
