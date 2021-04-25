f0f168abc4c7352cc72c747c3773967045265b0f
Elan Ruusamäe <glen@pld-linux.org>
2021-04-25 20:34:09 +0300

    Add make() helper

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import json
 from os.path import dirname, join as join_path
+from typing import Union
+
+from trakt.tv import TVShow
 
 from plex_trakt_sync.decorators import memoize
 
@@ -10,6 +13,12 @@ def load_mock(name: str):
     filename = join_path(MOCK_DATA_DIR, name)
     with open(filename, encoding='utf-8') as f:
         return json.load(f)
+
+
+def make(cls=None, **kwargs) -> Union[TVShow]:
+    cls = cls if cls is not None else "object"
+    # https://stackoverflow.com/a/2827726/2314626
+    return type(cls, (object,), kwargs)
 
 
 @memoize
