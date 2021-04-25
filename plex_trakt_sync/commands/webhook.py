8f66c0617f3bfdd5d36d230951c9a4c01dc93a41
Elan Ruusamäe <glen@pld-linux.org>
2021-04-25 23:13:01 +0300

    Add simple http server

--- plex_trakt_sync/commands/webhook.py
+++ plex_trakt_sync/commands/webhook.py
@@ -1,9 +1,18 @@
+import http.server
+import socketserver
+
 import click
 
 
 @click.command()
-def webhook():
+@click.option("--bind", help="Address to listen on", default="localhost")
+@click.option("--port", help="TCP port port to listen on", default=7707)
+def webhook(bind: str, port: int):
     """
     Listen for WebHook data from HTTP
     """
-    pass
+
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer((bind, port), Handler) as httpd:
+        click.echo(f"Serving at http://{bind}:{port}")
+        httpd.serve_forever()
