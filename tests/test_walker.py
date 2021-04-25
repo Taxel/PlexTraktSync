dfc27aab405cfa83d8795a132d11314c41eb688b
Elan Ruusamäe <glen@pld-linux.org>
2021-04-25 18:32:41 +0300

    Update Walker test with dependencies

--- tests/test_walker.py
+++ tests/test_walker.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3 -m pytest
 from plex_trakt_sync.walker import Walker
+from tests.conftest import get_plex_api, get_media_factory
+
+plex = get_plex_api()
+mf = get_media_factory()
 
 
 def test_walker():
-    w = Walker()
+    w = Walker(plex, mf)
 
     w.add_library("TV Shows")
     w.add_show("Breaking Bad")
