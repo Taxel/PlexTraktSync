07ce0f15ed7db39a121b7810e0d7a2b8290c1b05
Elan Ruusamäe <glen@pld-linux.org>
2021-04-26 08:17:50 +0300

    Show default options for usage

--- plex_trakt_sync/commands/webhook.py
+++ plex_trakt_sync/commands/webhook.py
@@ -92,8 +92,8 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
 
 
 @click.command()
-@click.option("--bind", help="Address to listen on", default="localhost")
-@click.option("--port", help="TCP port port to listen on", default=7707)
+@click.option("--bind", help="Address to listen on", show_default=True, default="localhost")
+@click.option("--port", help="TCP port to listen on", show_default=True, default=7707)
 def webhook(bind: str, port: int):
     """
     Listen for WebHook data from HTTP
