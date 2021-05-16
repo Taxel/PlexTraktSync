73ca9a842d2de876501baec8a2bc5676d2103664
Elan Ruusamäe <glen@pld-linux.org>
2021-05-16 10:36:11 +0300

    Add database access library
    Inspired by plexcleaner
    
    https://github.com/nap/plexcleaner/blob/853bb5b81325878071059b67a2cdad8d0b7311e5/plexcleaner/database.py

---
+++ plextraktsync/db/Database.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+from plextraktsync.factory import logging
+
+
+class Database:
+    _uncommited = False
+
+    def __init__(self, database_path: str):
+        self.logger = logging.getLogger("PlexTraktSync.Database")
+        try:
+            self.filename = database_path
+            self._connection = sqlite3.connect(database_path)
+            self._cursor = self._connection.cursor()
+            self._cursor.execute('ANALYZE')
+
+        except sqlite3.OperationalError as e:
+            self.logger.error(e)
+            raise e
+
+        except sqlite3.DatabaseError as e:
+            self.logger.error(e)
+            raise e
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._uncommited:
+            self.commit()
+
+        self._connection.close()
+
+    def commit(self):
+        self._connection.commit()
+        self._uncommited = False
+
+    def rollback(self):
+        self._connection.rollback()
+        self._uncommited = False
+
+    def has_uncommited(self):
+        return self._uncommited
