21b4e6a0b3cf66a53933c4f9b9cab9a7df3f0715
Elan Ruusamäe <glen@pld-linux.org>
2021-06-19 18:35:00 +0300

    Get requests_cache from factory

--- plex_trakt_sync/decorators/http_cache.py
+++ plex_trakt_sync/decorators/http_cache.py
@@ -1,14 +1,15 @@
 from functools import wraps
 
-import requests_cache
-
+from plex_trakt_sync.factory import factory
 from plex_trakt_sync.path import trakt_cache
+
+cache = factory.requests_cache()
 
 
 def http_cache(method, expire_after=None):
     @wraps(method)
     def inner(self, *args, **kwargs):
-        with requests_cache.enabled(trakt_cache, expire_after=expire_after):
+        with cache.enabled(trakt_cache, expire_after=expire_after):
             return method(self, *args, **kwargs)
 
     return inner
