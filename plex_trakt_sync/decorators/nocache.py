fabf79a5e2e9d68a5d9dc717abd5b237cd522aa0
Elan Ruusamäe <glen@pld-linux.org>
2021-06-19 17:03:14 +0300

    Update cache decorators to update session instance

--- plex_trakt_sync/decorators/nocache.py
+++ plex_trakt_sync/decorators/nocache.py
@@ -3,12 +3,14 @@ from functools import wraps
 from plex_trakt_sync.factory import factory
 
 cache = factory.requests_cache()
+session = factory.session()
 
 
 def nocache(method):
     @wraps(method)
     def inner(self, *args, **kwargs):
         with cache.disabled():
-            return method(self, *args, **kwargs)
+            with session.cache_disabled():
+                return method(self, *args, **kwargs)
 
     return inner
