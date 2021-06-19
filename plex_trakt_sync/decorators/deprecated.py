87c47ac96d1b0f51a5fb7f3d44866e84d2d52736
Elan Ruusamäe <glen@pld-linux.org>
2021-06-19 13:58:25 +0300

    Add @deprecated decorator
    Include a fallback to local nothing if package is not installed

---
+++ plex_trakt_sync/decorators/deprecated.py
@@ -0,0 +1,17 @@
+try:
+    from deprecated import deprecated
+except ImportError as e:
+    import warnings
+    from functools import wraps
+
+
+    def deprecated(reason=""):
+        def decorator(fn):
+            @wraps(fn)
+            def wrapper(*args, **kwargs):
+                warnings.warn(reason, DeprecationWarning, 2)
+                return fn(*args, **kwargs)
+
+            return wrapper
+
+        return decorator
