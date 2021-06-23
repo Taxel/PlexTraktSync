c9dd0eee23b6503f4e89a0c609f09a39ee66219f
Elan Ruusamäe <glen@pld-linux.org>
2021-06-23 20:50:10 +0300

    Cleanup unused imports

--- plex_trakt_sync/commands/watch.py
+++ plex_trakt_sync/commands/watch.py
@@ -1,9 +1,7 @@
 import click
-from plexapi.server import PlexServer
 
 from plex_trakt_sync.factory import factory
 from plex_trakt_sync.listener import WebSocketListener, PLAYING
-from plex_trakt_sync.config import CONFIG
 from plex_trakt_sync.plex_api import PlexApi
 from plex_trakt_sync.trakt_api import TraktApi
 
