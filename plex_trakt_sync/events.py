fcbd460757a1fde4598a1b279653c7cc3b9f22d9
Elan Ruusamäe <glen@pld-linux.org>
2021-06-23 20:50:26 +0300

    Add mapping of type to class

---
+++ plex_trakt_sync/events.py
@@ -0,0 +1,11 @@
+EVENTS = {
+    "account": "AccountUpdateNotification",
+    "activity": "ActivityNotification",
+    "backgroundProcessingQueue": "BackgroundProcessingQueueEventNotification",
+    "playing": "PlaySessionStateNotification",
+    "preference": "Setting",
+    "progress": "ProgressNotification",
+    "reachability": "ReachabilityNotification",
+    "status": "StatusNotification",
+    "timeline": "TimelineEntry",
+}
