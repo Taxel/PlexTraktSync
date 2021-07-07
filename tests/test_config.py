105197ae18bda4aa69436ca93fda620e0cea09ad
Elan Ruusamäe <glen@pld-linux.org>
2021-07-07 10:03:14 +0300

    Add test for config class

---
+++ tests/test_config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3 -m pytest
+from os import environ
+
+from plex_trakt_sync.factory import factory
+
+
+def test_config():
+    config = factory.config()
+
+    environ["PLEX_TOKEN"] = "Foo"
+    config.initialized = False
+    assert config["PLEX_TOKEN"] == "Foo"
+
+    try:
+        del environ["PLEX_TOKEN"]
+    except KeyError:
+        pass
+    config.initialized = False
+    assert config["PLEX_TOKEN"] is None
+
+    environ["PLEX_TOKEN"] = "-"
+    config.initialized = False
+    assert config["PLEX_TOKEN"] is None
+
+    environ["PLEX_TOKEN"] = "None"
+    config.initialized = False
+    assert config["PLEX_TOKEN"] is None
+
