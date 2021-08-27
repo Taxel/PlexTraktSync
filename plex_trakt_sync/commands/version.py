6af6cedb46883321b11a5bc68dae419576fb7c30
Elan Ruusamäe <glen@pld-linux.org>
2021-08-27 22:26:40 +0300

    Add command to print version info

---
+++ plex_trakt_sync/commands/version.py
@@ -0,0 +1,22 @@
+import sys
+
+import click
+from trakt import __version__ as TRAKT_API_VERSION
+from plexapi import VERSION as PLEX_API_VERSION
+
+from plex_trakt_sync.version import git_version_info
+
+
+@click.command()
+def version():
+    """
+    Print application and environment version info
+    """
+
+    git_version = git_version_info()
+    if git_version:
+        print(f"PlexTraktSync Git Version: [{git_version}]")
+
+    print(f"Python Version: {sys.version}")
+    print(f"Plex API Version: {PLEX_API_VERSION}")
+    print(f"Trakt API Version: {TRAKT_API_VERSION}")
