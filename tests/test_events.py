9cca7d21563cac881a28b145722e1fa55a1e82a4
Elan Ruusamäe <glen@pld-linux.org>
2021-09-25 14:37:26 +0300

    Add test for events-played.json data

--- tests/test_events.py
+++ tests/test_events.py
@@ -11,3 +11,17 @@ def test_events():
     for event in events:
         assert event["event"] == "ended"
         assert event["Activity"]["progress"] == 100
+
+
+def test_event_played():
+    event_factory = EventFactory()
+    raw_events = load_mock("events-played.json")
+
+    events = [list(event_factory.get_events(data))[0] for data in raw_events]
+    assert len(events) == 5
+    assert events[0]["event"] == "started"
+    assert events[1]["event"] == "updated"
+    assert events[4]["event"] == "ended"
+    assert events[4]["Activity"]["type"] == "library.refresh.items"
+    assert events[4]["Activity"]["progress"] == 100
+    assert events[4]["Activity"]["Context"]["key"] == "/library/metadata/513"
