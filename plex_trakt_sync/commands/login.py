fafbe2ac6b42878b6981fd75eb9d7e58eab924a8
Elan Ruusamäe <glen@pld-linux.org>
2021-10-13 00:17:35 +0300

    Avoid calling click commands from click commands

--- plex_trakt_sync/commands/login.py
+++ plex_trakt_sync/commands/login.py
@@ -1,14 +1,14 @@
 import click
 
-from plex_trakt_sync.commands.plex_login import has_plex_token, plex_login
-from plex_trakt_sync.commands.trakt_login import has_trakt_token, trakt_login
+from plex_trakt_sync.commands.plex_login import has_plex_token, plex_login_autoconfig
+from plex_trakt_sync.commands.trakt_login import has_trakt_token, trakt_login_autoconfig
 
 
 def ensure_login():
     if not has_plex_token():
-        plex_login()
+        plex_login_autoconfig()
     if not has_trakt_token():
-        trakt_login()
+        trakt_login_autoconfig()
 
 
 @click.command()
