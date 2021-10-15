5aa169c6a97b9fc2d6950ac801ae7395e7e31326
Elan Ruusamäe <glen@pld-linux.org>
2021-10-15 11:53:39 +0300

    Remove patching of global requests

--- plex_trakt_sync/decorators/nocache.py
+++ plex_trakt_sync/decorators/nocache.py
@@ -2,15 +2,13 @@ from functools import wraps
 
 from plex_trakt_sync.factory import factory
 
-cache = factory.requests_cache()
 session = factory.session()
 
 
 def nocache(method):
     @wraps(method)
     def inner(*args, **kwargs):
-        with cache.disabled():
-            with session.cache_disabled():
-                return method(*args, **kwargs)
+        with session.cache_disabled():
+            return method(*args, **kwargs)
 
     return inner
