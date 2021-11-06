c1dede21a835fb44afc805bca645d02e3e84d4b0
Elan Ruusamäe <glen@pld-linux.org>
2021-11-06 12:16:32 +0200

    Add handling of LockedUserAccountException

--- plextraktsync/decorators/rate_limit.py
+++ plextraktsync/decorators/rate_limit.py
@@ -2,7 +2,7 @@ from functools import wraps
 from time import sleep
 
 from requests import RequestException
-from trakt.errors import RateLimitException, TraktInternalException
+from trakt.errors import RateLimitException, TraktInternalException, LockedUserAccountException
 
 from plextraktsync.logging import logger
 
@@ -21,7 +21,7 @@ def rate_limit(retries=5):
             while True:
                 try:
                     return fn(*args, **kwargs)
-                except (RateLimitException, RequestException, TraktInternalException) as e:
+                except (RateLimitException, RequestException, TraktInternalException, LockedUserAccountException) as e:
                     if retry == retries:
                         logger.error(f"Error: {e}")
                         logger.error("API didn't respond properly, script will abort now. Please try again later.")
