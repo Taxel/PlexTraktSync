3adf2d60f0b44d6001696993154cb3aa34fd4b18
Elan Ruusamäe <glen@pld-linux.org>
2022-01-14 15:13:52 +0200

    Add ratings property being TraktRatingCollection to trakt_api

--- plextraktsync/trakt_api.py
+++ plextraktsync/trakt_api.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Union
 
 import trakt
@@ -11,6 +12,7 @@ from trakt.tv import TVEpisode, TVSeason, TVShow
 
 from plextraktsync import pytrakt_extensions
 from plextraktsync.decorators.cached_property import cached_property
+from plextraktsync.decorators.flatten import flatten_dict
 from plextraktsync.decorators.memoize import memoize
 from plextraktsync.decorators.nocache import nocache
 from plextraktsync.decorators.rate_limit import rate_limit
@@ -50,6 +52,24 @@ class ScrobblerProxy:
             self.scrobbler.stop()
         else:
             self.scrobbler.pause()
+
+
+class TraktRatingCollection(dict):
+    def __init__(self, trakt: TraktApi):
+        super(dict, self).__init__()
+        self.trakt = trakt
+
+    def __missing__(self, media_type: str):
+        ratings = self.ratings(media_type)
+        self[media_type] = ratings
+
+        return ratings
+
+    @flatten_dict
+    def ratings(self, media_type: str):
+        index = media_type.rstrip('s')
+        for r in self.trakt.me.get_ratings(media_type):
+            yield r[index]['ids']['trakt'], r['rating']
 
 
 class TraktApi:
@@ -129,6 +149,10 @@ class TraktApi:
     @rate_limit()
     def watchlist_movies(self):
         return self.me.watchlist_movies
+
+    @cached_property
+    def ratings(self):
+        return TraktRatingCollection(self)
 
     @cached_property
     @nocache
