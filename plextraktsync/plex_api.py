70a0f1c78e60d8e18effc310c1de64b6d0b12d9a
Elan Ruusamäe <glen@pld-linux.org>
2022-02-06 16:03:13 +0200

    Add PlexAudioCodec class

--- plextraktsync/plex_api.py
+++ plextraktsync/plex_api.py
@@ -135,6 +135,47 @@ class PlexRatingCollection(dict):
             yield item.ratingKey, item.userRating
 
 
+class PlexAudioCodec:
+    def match(self, codec):
+        for key, regex in self.audio_codecs.items():
+            if key == codec:
+                return key
+
+            if regex and regex.match(codec):
+                return key
+
+        return None
+
+    @cached_property
+    def audio_codecs(self):
+        codecs = {
+            'lpcm': 'pcm',
+            'mp3': None,
+            'aac': None,
+            'ogg': 'vorbis',
+            'wma': None,
+
+            'dts': '(dca|dta)',
+            'dts_ma': 'dtsma',
+
+            'dolby_prologic': 'dolby.?pro',
+            'dolby_digital': 'ac.?3',
+            'dolby_digital_plus': 'eac.?3',
+            'dolby_truehd': 'truehd'
+        }
+
+        # compile patterns
+        for k, v in codecs.items():
+            if v is None:
+                continue
+
+            try:
+                codecs[k] = re.compile(v, re.IGNORECASE)
+            except Exception:
+                raise RuntimeError('Unable to compile regex pattern: %r', v, exc_info=True)
+        return codecs
+
+
 class PlexLibraryItem:
     def __init__(self, item: Union[Movie, Show, Episode], plex: PlexApi = None):
         self.item = item
