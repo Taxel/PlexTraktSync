03ab93f83e00f6ce65f8300de9dc2d49a21050e6
Elan Ruusamäe <glen@pld-linux.org>
2022-07-24 01:36:34 +0300

    Build bug report url

--- plextraktsync/commands/bug_report.py
+++ plextraktsync/commands/bug_report.py
@@ -1,2 +1,20 @@
+from urllib.parse import urlencode
+
+URL_TEMPLATE = 'https://github.com/Taxel/PlexTraktSync/issues/new?template=bug.yml&{}'
+
+
+def bug_url():
+    from plextraktsync.util.versions import (pts_version, py_platform,
+                                             py_version)
+
+    q = urlencode({
+        'os': py_platform(),
+        'python': py_version(),
+        'version': pts_version(),
+    })
+
+    return URL_TEMPLATE.format(q)
+
+
 def bug_report():
     pass
