048b5e96544a3619d71f7eebc2686d348014b46c
Elan Ruusamäe <glen@pld-linux.org>
2022-07-24 01:58:06 +0300

    Implement openurl module

---
+++ plextraktsync/util/openurl.py
@@ -0,0 +1,22 @@
+# minimal version of openurl without six dependency
+# https://github.com/panda2134/open-python/blob/32a4b61b44302da185dcf6a72a48d1c726eb3e51/open_python/__init__.py
+import sys
+
+from plextraktsync.commands.self_update import execx
+
+
+def openurl(url: str):
+    try:
+        opener = {
+            'darwin': 'open',
+            'win32': 'start',
+        }[sys.platform]
+    except IndexError:
+        opener = 'xdg-open'
+
+    try:
+        execx([opener, url])
+    except FileNotFoundError:
+        return False
+
+    return True
