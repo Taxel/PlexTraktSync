bd035754b7e6cb09483a1617d1de3a3c0cba9aa0
Elan Ruusamäe <glen@pld-linux.org>
2022-08-30 10:23:40 +0300

    Add coloring to PlexLibrarySection

--- plextraktsync/rich_addons.py
+++ plextraktsync/rich_addons.py
@@ -9,7 +9,7 @@ class RichHighlighter(RegexHighlighter):
         r"(?P<number>(?<!\w)\-?[0-9]+\.?[0-9]*(e[\-\+]?\d+?)?\b|0x[0-9a-fA-F]*)",
         r"(?P<attrib_name>[\w_]{1,50})=(?P<attrib_value>\"?[\w_]+\"?)?",
         r"(?P<tag_start>\<)(?P<tag_name>(?:Movie|Episode|Show):\d+:[^>]+)(?P<tag_end>\>)",
-        r"(?P<tag_start>\<)(?P<tag_name>(?:PlexGuid|Guid):[^>]+)(?P<tag_end>\>)",
+        r"(?P<tag_start>\<)(?P<tag_name>(?:PlexGuid|Guid|PlexLibrarySection):[^>]+)(?P<tag_end>\>)",
         r"(?P<tag_start>\<)(?P<tag_name>(?:imdb|tmdb|tvdb|local):(?:\d+:)[^>]+)(?P<tag_end>\>)",
         r"\b(?P<bool_true>True)\b|\b(?P<bool_false>False)\b|\b(?P<none>None)\b",
         r"(?<![\\\w])(?P<str>b?\'\'\'.*?(?<!\\)\'\'\'|b?\'.*?(?<!\\)\'|b?\"\"\".*?(?<!\\)\"\"\"|b?\".*?(?<!\\)\")",
