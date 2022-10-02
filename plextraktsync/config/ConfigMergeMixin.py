254fc79389d24293618a84e566c9d892c46f113f
Elan Ruusamäe <glen@pld-linux.org>
2022-10-02 18:39:21 +0300

    Add ConfigMergeMixin

---
+++ plextraktsync/config/ConfigMergeMixin.py
@@ -0,0 +1,12 @@
+class ConfigMergeMixin:
+    # https://stackoverflow.com/a/20666342/2314626
+    def merge(self, source, destination):
+        for key, value in source.items():
+            if isinstance(value, dict):
+                # get node or create one
+                node = destination.setdefault(key, {})
+                self.merge(value, node)
+            else:
+                destination[key] = value
+
+        return destination
