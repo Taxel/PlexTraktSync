b3c7dc3f2fe3fa113874950a71ed95018315104e
Elan Ruusamäe <glen@pld-linux.org>
2022-10-06 18:31:01 +0300

    Add typing to trakt.search_by_id return value

--- plextraktsync/trakt_api.py
+++ plextraktsync/trakt_api.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from time import time
-from typing import Union
+from typing import List, Optional, Union
 
 import trakt
 import trakt.movies
@@ -309,7 +309,7 @@ class TraktApi:
 
     @rate_limit()
     @retry()
-    def search_by_id(self, media_id: str, id_type: str, media_type: str):
+    def search_by_id(self, media_id: str, id_type: str, media_type: str) -> Optional[List[TVShow, Movie]]:
         if id_type == "tvdb" and media_type == "movie":
             # Skip invalid search.
             # The Trakt API states that tvdb is only for shows and episodes:
