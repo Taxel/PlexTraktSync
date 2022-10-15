71241c8d69f7954284f3214c2b053d9a90249da0
Elan Ruusamäe <glen@pld-linux.org>
2022-10-15 12:20:09 +0300

    Update LoggerFilter to lazy init rules

--- plextraktsync/logger/filter.py
+++ plextraktsync/logger/filter.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from logging import Filter, Logger, LogRecord
 from typing import Dict, List
 
+from plextraktsync.decorators.cached_property import cached_property
+
 
 @dataclass
 class FilterRule:
@@ -20,12 +22,16 @@ class LoggerFilter(Filter):
     def __init__(self, rules: List[Dict], logger: Logger):
         super().__init__()
         self.logger = logger
-        self.rules = self.load_rules(rules or [])
-        self.nrules = len(self.rules)
+        self._rules = rules or []
 
-    def load_rules(self, rules: List[Dict]):
+    @cached_property
+    def nrules(self):
+        return len(self.rules)
+
+    @cached_property
+    def rules(self):
         filters = []
-        for rule in rules:
+        for rule in self._rules:
             try:
                 f = FilterRule(**rule)
             except TypeError as e:
