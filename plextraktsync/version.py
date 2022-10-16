6cb0c9eb716ac8e5c10612a7d324bea6381cbf34
Elan Ruusamäe <glen@pld-linux.org>
2022-10-17 02:10:01 +0300

    Extract version function to Version class

--- plextraktsync/version.py
+++ plextraktsync/version.py
@@ -1,3 +1,22 @@
+from plextraktsync.decorators.cached_property import cached_property
+
+
+class Version:
+    @cached_property
+    def version(self):
+        from plextraktsync import __version__
+
+        # Released in PyPI
+        if not __version__.endswith(".0dev0"):
+            return __version__
+
+        gv = git_version_info()
+        if gv:
+            return f"{__version__}: {gv}"
+
+        return __version__
+
+
 def git_version_info():
     try:
         from gitinfo import get_git_info
@@ -13,15 +32,8 @@ def git_version_info():
     return f"{commit['commit'][0:8]}: {message} @{commit['author_date']}"
 
 
+VERSION = Version()
+
+
 def version():
-    from plextraktsync import __version__
-
-    # Released in PyPI
-    if not __version__.endswith(".0dev0"):
-        return __version__
-
-    gv = git_version_info()
-    if gv:
-        return f"{__version__}: {gv}"
-
-    return __version__
+    return VERSION.version
