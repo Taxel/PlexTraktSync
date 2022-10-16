a45354530f87df203ca22b3d9431f337e69de73d
Elan Ruusamäe <glen@pld-linux.org>
2022-10-17 02:19:07 +0300

    Extract pipx_installed to standalone

--- plextraktsync/util/packaging.py
+++ plextraktsync/util/packaging.py
@@ -1,5 +1,9 @@
+import json
 import site
+from json import JSONDecodeError
 from os.path import dirname
+
+from plextraktsync.util.execx import execx
 
 
 def installed():
@@ -10,3 +14,26 @@ def installed():
     paths = site.getsitepackages()
 
     return absdir in paths
+
+
+def pipx_installed(package: str):
+    try:
+        output = execx("pipx list --json")
+    except FileNotFoundError:
+        return None
+    if not output:
+        return None
+
+    try:
+        install_data = json.loads(output)
+    except JSONDecodeError:
+        return None
+    if install_data is None:
+        return None
+
+    try:
+        package = install_data["venvs"][package]["metadata"]["main_package"]
+    except KeyError:
+        return None
+
+    return package
