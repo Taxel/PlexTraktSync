a345364c5926659fc090ec69883a7e08eb1bccc6
Elan Ruusamäe <glen@pld-linux.org>
2022-10-26 00:11:57 +0300

    Load session for @nocache at callout time
    This avoids initializing session too early

--- plextraktsync/decorators/nocache.py
+++ plextraktsync/decorators/nocache.py
@@ -2,12 +2,11 @@ from functools import wraps
 
 from plextraktsync.factory import factory
 
-session = factory.session()
-
 
 def nocache(method):
     @wraps(method)
     def inner(*args, **kwargs):
+        session = factory.session()
         with session.cache_disabled():
             return method(*args, **kwargs)
 
