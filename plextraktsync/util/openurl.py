d6f6db5f2267822447dd99ba250aece14aea2e1c
Elan Ruusamäe <glen@pld-linux.org>
2022-10-31 17:13:09 +0200

    Fix execx import in openurl

--- plextraktsync/util/openurl.py
+++ plextraktsync/util/openurl.py
@@ -2,7 +2,7 @@
 # https://github.com/panda2134/open-python/blob/32a4b61b44302da185dcf6a72a48d1c726eb3e51/open_python/__init__.py
 import sys
 
-from plextraktsync.commands.self_update import execx
+from plextraktsync.util.execx import execx
 
 
 def openurl(url: str):
