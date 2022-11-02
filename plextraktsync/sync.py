e8199c966ef889bb42ce6bf382d49e2d21a3fe6e
Elan Ruusamäe <glen@pld-linux.org>
2022-11-02 22:14:12 +0200

    Add new properties to SyncConfig
    - Add sync_watchlists SyncConfig property
    - Add update_plex_wl SyncConfig property
    - Add update_plex_wl_as_pl SyncConfig property
    - Add update_trakt_wl SyncConfig property
    - Add sync_wl SyncConfig property
    - Add need_library_walk SyncConfig property

--- plextraktsync/sync.py
+++ plextraktsync/sync.py
@@ -46,10 +46,40 @@ class SyncConfig:
         return self.trakt_to_plex["ratings"] or self.plex_to_trakt["ratings"]
 
     @cached_property
+    def sync_watchlists(self):
+        return self.trakt_to_plex["watchlist"] or self.plex_to_trakt["watchlist"]
+
+    @cached_property
     def sync_watched_status(self):
         return (
             self.trakt_to_plex["watched_status"] or self.plex_to_trakt["watched_status"]
         )
+
+    @cached_property
+    def update_plex_wl(self):
+        return self.trakt_to_plex["watchlist"] and not self.trakt_to_plex["watchlist_as_playlist"]
+
+    @cached_property
+    def update_plex_wl_as_pl(self):
+        return self.trakt_to_plex["watchlist"] and self.trakt_to_plex["watchlist_as_playlist"]
+
+    @cached_property
+    def update_trakt_wl(self):
+        return self.plex_to_trakt["watchlist"]
+
+    @cached_property
+    def sync_wl(self):
+        return self.trakt_to_plex["watchlist"] or self.plex_to_trakt["watchlist"]
+
+    @cached_property
+    def need_library_walk(self):
+        return any([
+            self.update_plex_wl_as_pl,
+            self.sync_watched_status,
+            self.sync_ratings,
+            self.plex_to_trakt["collection"],
+            self.trakt_to_plex["liked_lists"],
+        ])
 
 
 class Sync:
