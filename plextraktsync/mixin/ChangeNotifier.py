89a6bb9229c2186f712a47bdc4ed78bb42d94485
Elan Ruusamäe <glen@pld-linux.org>
2022-12-01 16:11:14 +0200

    Add ChangeNotifier MixIn

---
+++ plextraktsync/mixin/ChangeNotifier.py
@@ -0,0 +1,19 @@
+class ChangeNotifier(dict):
+    """
+    MixIn that would make dict object notify listeners when a value is set to dict
+    """
+
+    listeners = []
+
+    def add_listener(self, listener, keys=None):
+        self.listeners.append((listener, keys))
+
+    def notify(self, key, value):
+        for listener, keys in self.listeners:
+            if keys is not None and key not in keys:
+                continue
+            listener(key, value)
+
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, key, value)
+        self.notify(key, value)
