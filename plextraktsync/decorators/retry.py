4a07963f22151fd2c59ce4b1b64e85ab724f76c9
Elan Ruusamäe <glen@pld-linux.org>
2022-12-12 11:16:21 +0200

    Catch TraktUnavailable in retry

--- plextraktsync/decorators/retry.py
+++ plextraktsync/decorators/retry.py
@@ -4,7 +4,7 @@ from time import sleep
 from plexapi.exceptions import BadRequest
 from requests import ReadTimeout, RequestException
 from trakt.errors import (BadResponseException, TraktBadGateway,
-                          TraktInternalException)
+                          TraktInternalException, TraktUnavailable)
 
 from plextraktsync.factory import logger
 
@@ -30,6 +30,7 @@ def retry(retries=5):
                         ReadTimeout,
                         RequestException,
                         TraktBadGateway,
+                        TraktUnavailable,
                         TraktInternalException,
                 ) as e:
                     if count == retries:
