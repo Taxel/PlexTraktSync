7c38c8ecd02e32f818ad1dd87bace94872b0983b
Elan Ruusamäe <glen@pld-linux.org>
2022-12-14 12:32:52 +0200

    Deprecate --server for sync command

--- plextraktsync/commands/sync.py
+++ plextraktsync/commands/sync.py
@@ -29,10 +29,12 @@ def sync(
     shows = sync_option in ["all", "tv", "shows"]
 
     config = factory.run_config.update(
-        server=server,
         batch_delay=batch_delay,
         dry_run=dry_run,
     )
+    if server:
+        logger.warning('"plextraktsync sync --server=<name>" is deprecated use "plextraktsync --server=<name> sync"')
+        config.update(server=server)
     if no_progress_bar:
         logger.warning('"plextraktsync sync --no-progress-bar" is deprecated use "plextraktsync --no-progressbar sync"')
         config.update(progress=False)
