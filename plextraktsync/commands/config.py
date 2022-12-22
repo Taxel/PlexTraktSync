21ebde15604ebd5fd00490e78863b5f7f8b52b4e
Elan Ruusamäe <glen@pld-linux.org>
2022-12-22 15:56:59 +0200

    Add generic dump to config command

--- plextraktsync/commands/config.py
+++ plextraktsync/commands/config.py
@@ -1,6 +1,19 @@
 from plextraktsync.console import print
 
 
+def dump(data, print=None):
+    """
+    Print config serialized as yaml.
+    If print is None, return the produced string instead.
+    """
+    from plextraktsync.config.ConfigLoader import ConfigLoader
+
+    dump = ConfigLoader.dump_yaml(None, data)
+    if print is None:
+        return dump
+    print(dump)
+
+
 def config(print=print):
     from plextraktsync.factory import factory
     config = factory.config
