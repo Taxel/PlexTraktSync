1f74fe0ea15359e6c82a02bab3d46d51c76b6de4
Elan Ruusamäe <glen@pld-linux.org>
2023-01-05 01:01:25 +0200

    Remove plextraktsync.plex_api module

--- plextraktsync/plex_api.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from plextraktsync.plex.PlexApi import PlexApi  # noqa
-from plextraktsync.plex.PlexGuid import PlexGuid  # noqa
-from plextraktsync.plex.PlexLibraryItem import PlexLibraryItem  # noqa
-from plextraktsync.plex.PlexLibrarySection import PlexLibrarySection  # noqa
