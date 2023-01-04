7a47793ecaf5c851684023ac4d0350eccdfe66aa
Elan Ruusamäe <glen@pld-linux.org>
2023-01-05 00:59:02 +0200

    Remove plextraktsync.trakt_api module

--- plextraktsync/trakt_api.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from plextraktsync.trakt.TraktApi import TraktApi  # noqa
-from plextraktsync.trakt.TraktBatch import TraktBatch  # noqa
-from plextraktsync.trakt.TraktItem import TraktItem  # noqa
-from plextraktsync.trakt.TraktLookup import TraktLookup  # noqa
