8dc8253bdea992a743ba7d11a1dbe80936567117
Elan Ruusamäe <glen@pld-linux.org>
2023-01-13 01:44:40 +0200

    Add filter to builtin loggers too

--- plextraktsync/factory.py
+++ plextraktsync/factory.py
@@ -208,12 +208,23 @@ class Factory:
     def logger(self):
         logger = self.logging.getLogger("PlexTraktSync")
         config = self.config
+        loggers = [
+            "PlexTraktSync",
+            "PlexTraktSync.BackgroundTask",
+            "PlexTraktSync.EventDispatcher",
+            "PlexTraktSync.PlexServerConnection",
+            "PlexTraktSync.ScrobblerProxy",
+            "PlexTraktSync.Timer",
+            "PlexTraktSync.TraktBatchWorker",
+            "PlexTraktSync.WatchStateUpdater",
+            "PlexTraktSync.WebSocketListener",
+        ]
+        loggers.extend(config["logging"]["filter_loggers"] or [])
 
         from plextraktsync.logger.filter import LoggerFilter
         filter = LoggerFilter(config["logging"]["filter"], logger)
-        logger.addFilter(filter)
 
-        for name in config["logging"]["filter_loggers"] or []:
+        for name in loggers:
             self.logging.getLogger(name).addFilter(filter)
 
         return logger
