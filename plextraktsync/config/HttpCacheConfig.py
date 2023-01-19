70bca57db79103b10fab474d23dc415941c48a8e
Elan Ruusamäe <glen@pld-linux.org>
2023-01-19 19:32:44 +0200

    Add rule for trakt.tv/shows/*/seasons?extended=episodes

--- plextraktsync/config/HttpCacheConfig.py
+++ plextraktsync/config/HttpCacheConfig.py
@@ -33,6 +33,7 @@ class HttpCacheConfig:
     default_policy = {
         # Requests matching these patterns will not be cached
         "*.trakt.tv/shows/*/seasons": DO_NOT_CACHE,
+        "*.trakt.tv/shows/*/seasons?extended=episodes": "1m",
         "*.trakt.tv/sync/collection/shows": "1m",
         "*.trakt.tv/sync/watched/shows": "1m",
         "*.trakt.tv/users/*/collection/movies?extended=metadata": "1m",
