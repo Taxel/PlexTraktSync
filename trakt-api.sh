c61cd599ed8ac431159aabb242ae46f068a269e0
Elan Ruusamäe <glen@pld-linux.org>
2023-04-12 09:20:00 +0300

    Add helper script to make trakt api requests
    Accepts TRAKT_API_KEY, TRAKT_AUTHORIZATION env variables,
    Reads tokens form .pytrakt.json if needed with jq

---
+++ trakt-api.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+: ${TRAKT_API_KEY=$(jq -r .CLIENT_ID < .pytrakt.json)}
+: ${TRAKT_AUTHORIZATION=Bearer $(jq -r .OAUTH_TOKEN < .pytrakt.json)}
+
+curl -sSf \
+     --header "Content-Type: application/json" \
+     --header "trakt-api-version: 2" \
+     --header "trakt-api-key: $TRAKT_API_KEY" \
+     --header "Authorization: $TRAKT_AUTHORIZATION" \
+	 "$@"
