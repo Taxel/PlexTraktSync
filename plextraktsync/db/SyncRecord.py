a4cacfade72de56b0278415162e1775effa67367
Elan Ruusamäe <glen@pld-linux.org>
2023-04-30 01:29:30 +0300

    Convert SyncRecord to SQLModel

--- plextraktsync/db/SyncRecord.py
+++ plextraktsync/db/SyncRecord.py
@@ -1,13 +1,13 @@
-from dataclasses import dataclass
+from __future__ import annotations
+
+from sqlmodel import Field, SQLModel
 
 
-@dataclass
-class SyncRecord:
+class SyncRecord(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     media_id: str
     plex_timestamp_watched: str
     seen_on_plex_sync: str
     trakt_timestamp_watched: str
     seen_on_trakt_sync: str
     result: str
-
-    id: str = None
