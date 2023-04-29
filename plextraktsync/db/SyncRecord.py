797de3d31363dc1df2a6e11daff36b36b3c6c196
Elan Ruusamäe <glen@pld-linux.org>
2023-04-29 13:08:08 +0300

    Add SyncRecord class

---
+++ plextraktsync/db/SyncRecord.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class SyncRecord:
+    id: str
+    media_id: str
+    plex_timestamp_watched: str
+    seen_on_plex_sync: str
+    trakt_timestamp_watched: str
+    seen_on_trakt_sync: str
+    result: str
