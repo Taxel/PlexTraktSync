93f409ef546855c086cec18d969c048fa3016114
Elan Ruusamäe <glen@pld-linux.org>
2023-04-29 13:25:34 +0300

    Mark id optional

--- plextraktsync/db/SyncRecord.py
+++ plextraktsync/db/SyncRecord.py
@@ -3,10 +3,11 @@ from dataclasses import dataclass
 
 @dataclass
 class SyncRecord:
-    id: str
     media_id: str
     plex_timestamp_watched: str
     seen_on_plex_sync: str
     trakt_timestamp_watched: str
     seen_on_trakt_sync: str
     result: str
+
+    id: str = None
