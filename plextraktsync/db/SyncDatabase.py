f1084461d7cf4c3c652dd6e8f5d58c6411b960da
Elan Ruusamäe <glen@pld-linux.org>
2023-04-29 13:09:55 +0300

    fixup! Create table on connection

--- plextraktsync/db/SyncDatabase.py
+++ plextraktsync/db/SyncDatabase.py
@@ -33,8 +33,8 @@ class SyncDatabase:
             self._create_table(con)
 
     # Initial CREATE TABLE must happen in shared connection; subsequent queries will use thread-local connections
-    def _create_table(self, connection: Database):
-        connection.execute(f'CREATE TABLE IF NOT EXISTS {self.table_name} ({self.schema})')
+    def _create_table(self, con: Database):
+        con.execute(f'CREATE TABLE IF NOT EXISTS {self.table_name} ({self.schema})')
 
     def insert(self, record: SyncRecord):
         pass
