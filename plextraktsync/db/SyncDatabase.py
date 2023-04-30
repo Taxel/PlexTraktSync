d7ef79006f5753bab8647bae1715b1b5ee2e3dac
Elan Ruusamäe <glen@pld-linux.org>
2023-04-30 17:11:56 +0300

    SyncDatabase switch to engine

--- plextraktsync/db/SyncDatabase.py
+++ plextraktsync/db/SyncDatabase.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from plextraktsync.db.Database import Database
 from plextraktsync.db.models.SyncRecord import SyncRecord
 
 if TYPE_CHECKING:
@@ -10,32 +9,8 @@ if TYPE_CHECKING:
 
 
 class SyncDatabase:
-    table_name = "sync"
-    # fields:
-    # A. Media ID
-    # B. Plex timestamp watched
-    # C. seen on Plex sync?
-    # D. Trakt timestamp watched
-    # E. seen on Trakt sync?
-    # F. result
-    schema = """
-        id PRIMARY KEY,
-        media_id,
-        plex_timestamp_watched,
-        seen_on_plex_sync,
-        trakt_timestamp_watched,
-        seen_on_trakt_sync,
-        result
-    """
-
-    def __init__(self, con: Database):
-        self.con = con
-        with self.con as con:
-            self._create_table(con)
-
-    # Initial CREATE TABLE must happen in shared connection; subsequent queries will use thread-local connections
-    def _create_table(self, con: Database):
-        con.execute(f'CREATE TABLE IF NOT EXISTS {self.table_name} ({self.schema})')
+    def __init__(self, engine):
+        self.engine = engine
 
     def insert(self, record: SyncRecord):
         pass
