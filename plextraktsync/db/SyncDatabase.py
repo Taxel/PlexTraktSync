989b5ff01ccd4694b4b39b0f2c8a86b520bd00a3
Elan Ruusamäe <glen@pld-linux.org>
2023-04-30 17:16:17 +0300

    SyncDatabase: Add find_by_id method

--- plextraktsync/db/SyncDatabase.py
+++ plextraktsync/db/SyncDatabase.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from sqlmodel import Session, select
+
 from plextraktsync.db.models.SyncRecord import SyncRecord
 
 if TYPE_CHECKING:
@@ -13,6 +15,15 @@ if TYPE_CHECKING:
 class SyncDatabase:
     def __init__(self, engine: Engine):
         self.engine = engine
+
+    def find_by_id(self, media_type: str, trakt_id: int):
+        with Session(self.engine) as session:
+            statement = (
+                select(SyncRecord)
+                .where(SyncRecord.media_type == media_type)
+                .where(SyncRecord.trakt_id == trakt_id)
+            )
+            return session.exec(statement).first()
 
     def insert(self, record: SyncRecord):
         pass
