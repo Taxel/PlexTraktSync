6957031bb16a51ceb3c7ffb47611d78a7397823c
Elan Ruusamäe <glen@pld-linux.org>
2023-04-30 17:11:56 +0300

    Add SyncDatabase class

---
+++ plextraktsync/db/SyncDatabase.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from plextraktsync.db.models.SyncRecord import SyncRecord
+
+if TYPE_CHECKING:
+    from sqlalchemy.future import Engine
+
+    from plextraktsync.media import Media
+
+
+class SyncDatabase:
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def insert(self, record: SyncRecord):
+        pass
+
+    def update(self, m: Media):
+        record = SyncRecord(
+            media_id=m.trakt_id,
+            plex_timestamp_watched=m.watched_on_plex,
+            seen_on_plex_sync=m.watched_on_plex,
+            trakt_timestamp_watched=m.watched_on_trakt,
+            seen_on_trakt_sync=m.watched_on_trakt,
+            result="",
+        )
+        print(record)
