4e1c8b6222245aa212efa2a39729e6c5a4f557b4
Elan Ruusamäe <glen@pld-linux.org>
2023-05-02 08:13:03 +0300

    Add TraktLikedList type

--- plextraktsync/trakt/types.py
+++ plextraktsync/trakt/types.py
@@ -1,6 +1,11 @@
-from typing import Union
+from typing import TypedDict, Union
 
 from trakt.movies import Movie
 from trakt.tv import TVEpisode, TVSeason, TVShow
 
 TraktMedia = Union[Movie, TVShow, TVSeason, TVEpisode]
+
+
+class TraktLikedList(TypedDict):
+    listid: int
+    listname: str
