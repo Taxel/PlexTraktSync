da216ae11c01ed90022eede03074a57c1ff90398
Elan Ruusamäe <glen@pld-linux.org>
2023-12-05 19:54:40 +0200

    Setup importable module

--- plextraktsync/logger/systemd.py
+++ plextraktsync/logger/systemd.py
@@ -1,37 +1,8 @@
-import logging
-import random
-import time
+systemd_handler = None
 
 try:
     from cysystemd.journal import JournaldLogHandler
+
+    systemd_handler = JournaldLogHandler
 except ImportError:
     pass
-
-# get an instance of the logger object this module will use
-logger = logging.getLogger(__name__)
-
-# instantiate the JournaldLogHandler to hook into systemd
-journald_handler = JournaldLogHandler()
-
-# set a formatter to include the level name
-journald_handler.setFormatter(logging.Formatter(
-    '[%(levelname)s] %(message)s'
-))
-
-# add the journald handler to the current logger
-logger.addHandler(journald_handler)
-
-# optionally set the logging level
-logger.setLevel(logging.DEBUG)
-
-if __name__ == '__main__':
-    print("logger:", logger)
-    while True:
-        # log a sample event
-        logger.info(
-            'test log event to systemd! Random number: %s',
-            random.randint(0, 10)
-        )
-
-        # sleep for some time to not saturate the journal
-        time.sleep(5)
