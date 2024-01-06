2f197de62ca5493d2ee2040fead2463f5940f340
Elan Ruusamäe <glen@pld-linux.org>
2024-01-06 12:10:38 +0200

    Add libraries and excluded_libraries methods to PlexServerConfig

--- plextraktsync/config/PlexServerConfig.py
+++ plextraktsync/config/PlexServerConfig.py
@@ -28,3 +28,17 @@ class PlexServerConfig:
             return {}
 
         return self.config["sync"]
+
+    @property
+    def libraries(self):
+        if self.config is None or "libraries" not in self.config:
+            return None
+
+        return self.config["libraries"]
+
+    @property
+    def excluded_libraries(self):
+        if self.config is None or "excluded-libraries" not in self.config:
+            return None
+
+        return self.config["excluded-libraries"]
