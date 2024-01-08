84a6094cf79efbf11532a8f4b16c4c7a49655f40
Elan Ruusamäe <glen@pld-linux.org>
2024-01-08 21:20:02 +0200

    Remove unused TraktList

--- plextraktsync/trakt_list_util.py
+++ plextraktsync/trakt_list_util.py
@@ -1,12 +1,7 @@
 from __future__ import annotations
 
-from itertools import count
-
-from plexapi.video import Episode
 from trakt.core import get
 from trakt.users import UserList
-
-from plextraktsync.factory import logger
 
 
 class LazyUserList(UserList):
@@ -28,52 +23,3 @@ class LazyUserList(UserList):
         ulist = cls(creator=data['user']['username'], **data)
         ulist.get_items()
         yield ulist
-
-
-class TraktList:
-    def __init__(self, listid, listname):
-        self.name = listname
-        self.plex_items = []
-        self.description = None
-        if listid is not None:
-            userlist = LazyUserList._get(listname, listid)
-            self.description = userlist.description
-            list_items = userlist._items
-            prelist = [
-                (elem[0], elem[1])
-                for elem in list_items
-                if elem[0] in ["movies", "episodes"]
-            ]
-            self.trakt_items = dict(zip(prelist, count(1)))
-
-    @property
-    def plex_items_sorted(self):
-        """
-        Returns items sorted by trakt rank
-
-        https://github.com/Taxel/PlexTraktSync/pull/58
-        """
-        if len(self.plex_items) == 0:
-            return []
-
-        _, items = zip(*sorted(dict(reversed(self.plex_items)).items()))
-        return items
-
-    @staticmethod
-    def from_trakt_list(listname, trakt_list):
-        tl = TraktList(None, listname)
-        tl.trakt_items = dict(
-            zip([(elem.media_type, elem.trakt) for elem in trakt_list], count(1))
-        )
-        return tl
-
-    def addPlexItem(self, trakt_item, plex_item):
-        rank = self.trakt_items.get((trakt_item.media_type, trakt_item.trakt))
-        if rank is not None:
-            self.plex_items.append((rank, plex_item))
-            if isinstance(plex_item, Episode):
-                logger.info(
-                    f"Added to list '{self.name}': {plex_item.show()}: {plex_item.seasonEpisode}"
-                )
-            else:
-                logger.info(f"Added to list '{self.name}': {plex_item}")
