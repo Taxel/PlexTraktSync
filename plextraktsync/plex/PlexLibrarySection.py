eee246406ed40fb73916461bfcb77de8eaf83850
Elan Ruusamäe <glen@pld-linux.org>
2024-01-14 22:50:24 +0200

    Remove unused pager related code from PlexLibrarySection

--- plextraktsync/plex/PlexLibrarySection.py
+++ plextraktsync/plex/PlexLibrarySection.py
@@ -2,12 +2,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from plexapi import X_PLEX_CONTAINER_SIZE
 from plexapi.exceptions import NotFound
 
-from plextraktsync.decorators.retry import retry
 from plextraktsync.mixin.RichMarkup import RichMarkup
-from plextraktsync.plex.PlexLibraryItem import PlexLibraryItem
 
 if TYPE_CHECKING:
     from typing import Literal
@@ -61,39 +58,6 @@ class PlexLibrarySection(RichMarkup):
             return self.section.fetchItem(int(id))
         except NotFound:
             return None
-
-    def search_episodes(self):
-        if self.section.type == "show":
-            from plextraktsync.plex.PlexSectionPager import PlexSectionPager
-
-            return PlexSectionPager(section=self.section, plex=self.plex)
-
-        return None
-
-    def all(self, max_items: int):
-        libtype = self.section.TYPE
-        key = self.section._buildSearchKey(libtype=libtype, returnKwargs=False)
-        start = 0
-        size = X_PLEX_CONTAINER_SIZE
-
-        while True:
-            items = self.fetch_items(key, size, start)
-            if not len(items):
-                break
-
-            yield from items
-
-            start += size
-            if start > max_items:
-                break
-
-    @retry()
-    def fetch_items(self, key: str, size: int, start: int):
-        return self.section.fetchItems(key, container_start=start, container_size=size, maxresults=size)
-
-    def items(self, max_items: int):
-        for item in self.all(max_items):
-            yield PlexLibraryItem(item, plex=self.plex)
 
     def __repr__(self):
         return f"<{self.__class__.__name__}:{self.type}:{self.title}>"
