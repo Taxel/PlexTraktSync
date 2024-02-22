0d56f40fc545adec8bb9966842e05842a19776f9
Elan Ruusamäe <glen@pld-linux.org>
2024-02-22 12:46:19 +0200

    Add test for Rating class

---
+++ tests/test_rating.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 -m pytest
+from datetime import datetime
+
+from plextraktsync.util.Rating import Rating
+
+
+def test_rating():
+    r = Rating.create(1.0, datetime(2024, 1, 17, 2, 38, 49))
+    assert r.rating == 1
+    assert r.rated_at == datetime(2024, 1, 17, 2, 38, 49)
