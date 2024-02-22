4b840441388e0b7090cf01c8ea0cfbf23d21561d
Elan Ruusamäe <glen@pld-linux.org>
2024-02-22 12:47:54 +0200

    Add None test

--- tests/test_rating.py
+++ tests/test_rating.py
@@ -5,6 +5,9 @@ from plextraktsync.util.Rating import Rating
 
 
 def test_rating():
+    r = Rating.create(None, datetime(2024, 1, 17, 2, 38, 49))
+    assert r is None
+
     r = Rating.create(1.0, datetime(2024, 1, 17, 2, 38, 49))
     assert r.rating == 1
     assert r.rated_at == datetime(2024, 1, 17, 2, 38, 49)
