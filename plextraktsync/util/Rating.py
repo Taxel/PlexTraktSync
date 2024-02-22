5db4cf4f743db2f8a7a6abe798353841d67f14c4
Elan Ruusamäe <glen@pld-linux.org>
2024-02-22 12:13:29 +0200

    Add eq operator to Rating class

--- plextraktsync/util/Rating.py
+++ plextraktsync/util/Rating.py
@@ -7,3 +7,7 @@ from typing import NamedTuple
 class Rating(NamedTuple):
     rating: int
     rated_at: datetime
+
+    def __eq__(self, other):
+        """ Ratings are equal if their rating value is the same """
+        return self.rating == other.rating
