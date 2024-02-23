8a4c8d3dec061b9f8675d0f1d0e3ec287834c432
Elan Ruusamäe <glen@pld-linux.org>
2024-02-23 17:21:17 +0200

    Filter liked list items with keep_watched

--- plextraktsync/trakt/TraktUserListCollection.py
+++ plextraktsync/trakt/TraktUserListCollection.py
@@ -23,6 +23,8 @@ class TraktUserListCollection(UserList):
         # https://support.plex.tv/articles/multiple-editions/#:~:text=Do%20Multiple%20Editions%20work%20with%20watch%20state%20syncing%3F
         if m.plex.edition_title is not None:
             return
+        if not self.keep_watched and m.plex.is_watched:
+            return
         for tl in self:
             tl.add(m)
 
@@ -42,7 +44,7 @@ class TraktUserListCollection(UserList):
 
     def sync(self):
         for tl in self:
-            updated = tl.plex_list.update(tl.plex_items_sorted)
+            updated = tl.plex_list.update(tl.plex_items_sorted(self.keep_watched))
             if not updated:
                 continue
             self.logger.info(f"Plex list {tl.title_link} ({len(tl.plex_items)} items) updated", extra={"markup": True})
