510d33af96b637f6daaf2027b23e3c3050d41989
Elan Ruusamäe <glen@pld-linux.org>
2024-02-23 10:33:34 +0200

    Use named logger in Sync class

--- plextraktsync/sync/Sync.py
+++ plextraktsync/sync/Sync.py
@@ -4,7 +4,7 @@ from functools import cached_property
 from typing import TYPE_CHECKING
 
 from plextraktsync.decorators.measure_time import measure_time
-from plextraktsync.factory import logger
+from plextraktsync.factory import logging
 from plextraktsync.trakt.TraktUserListCollection import TraktUserListCollection
 
 if TYPE_CHECKING:
@@ -23,6 +23,7 @@ class Sync:
         self.config = config
         self.plex = plex
         self.trakt = trakt
+        self.logger = logging.getLogger(__name__)
 
     @cached_property
     def plex_wl(self):
@@ -45,17 +46,17 @@ class Sync:
         is_partial = walker.is_partial and not dry_run
 
         if is_partial and self.config.clear_collected:
-            logger.warning("Running partial library sync. Clear collected will be disabled.")
+            self.logger.warning("Running partial library sync. Clear collected will be disabled.")
 
         if self.config.update_plex_wl_as_pl:
             if is_partial:
-                logger.warning("Running partial library sync. Watchlist as playlist won't update because it needs full library sync.")
+                self.logger.warning("Running partial library sync. Watchlist as playlist won't update because it needs full library sync.")
             else:
                 trakt_lists.add_watchlist(self.trakt.watchlist_movies)
 
         if self.config.sync_liked_lists:
             if is_partial:
-                logger.warning("Partial walk, disabling liked lists updating. Liked lists won't update because it needs full library sync.")
+                self.logger.warning("Partial walk, disabling liked lists updating. Liked lists won't update because it needs full library sync.")
             else:
                 trakt_lists.load_lists(self.trakt.liked_lists)
 
@@ -96,7 +97,7 @@ class Sync:
 
         if self.config.update_plex_wl_as_pl or self.config.sync_liked_lists:
             if is_partial:
-                logger.warning("Running partial library sync. Liked lists won't update because it needs full library sync.")
+                self.logger.warning("Running partial library sync. Liked lists won't update because it needs full library sync.")
             else:
                 if not dry_run:
                     with measure_time("Updated liked list"):
@@ -113,7 +114,7 @@ class Sync:
         if m.is_collected:
             return
 
-        logger.info(f"Adding to collection: {m.title_link}", extra={"markup": True})
+        self.logger.info(f"Adding to collection: {m.title_link}", extra={"markup": True})
 
         if not dry_run:
             m.add_to_collection()
@@ -154,12 +155,12 @@ class Sync:
                 rate = "plex"
 
         if rate == "trakt":
-            logger.info(f"Rating {m.title_link} with {m.plex_rating} on Trakt (was {m.trakt_rating})", extra={"markup": True})
+            self.logger.info(f"Rating {m.title_link} with {m.plex_rating} on Trakt (was {m.trakt_rating})", extra={"markup": True})
             if not dry_run:
                 m.trakt_rate()
 
         elif rate == "plex":
-            logger.info(f"Rating {m.title_link} with {m.trakt_rating} on Plex (was {m.plex_rating})", extra={"markup": True})
+            self.logger.info(f"Rating {m.title_link} with {m.trakt_rating} on Plex (was {m.plex_rating})", extra={"markup": True})
             if not dry_run:
                 m.plex_rate()
 
@@ -176,27 +177,27 @@ class Sync:
 
             if m.is_episode and m.watched_before_reset:
                 show = m.plex.item.show()
-                logger.info(f"Show '{show.title}' has been reset in trakt at {m.show_reset_at}.")
-                logger.info(f"Marking '{show.title}' as unwatched in Plex.")
+                self.logger.info(f"Show '{show.title}' has been reset in trakt at {m.show_reset_at}.")
+                self.logger.info(f"Marking '{show.title}' as unwatched in Plex.")
                 if not dry_run:
                     m.reset_show()
             else:
-                logger.info(f"Marking as watched in Trakt: {m.title_link}", extra={"markup": True})
+                self.logger.info(f"Marking as watched in Trakt: {m.title_link}", extra={"markup": True})
                 if not dry_run:
                     m.mark_watched_trakt()
         elif m.watched_on_trakt:
             if not self.config.trakt_to_plex["watched_status"]:
                 return
-            logger.info(f"Marking as watched in Plex: {m.title_link}", extra={"markup": True})
+            self.logger.info(f"Marking as watched in Plex: {m.title_link}", extra={"markup": True})
             if not dry_run:
                 m.mark_watched_plex()
 
     def watchlist_sync_item(self, m: Media, dry_run=False):
         if m.plex is None:
             if self.config.update_plex_wl:
-                logger.info(f"Skipping {m.title_link} from Trakt watchlist because not found in Plex Discover", extra={"markup": True})
+                self.logger.info(f"Skipping {m.title_link} from Trakt watchlist because not found in Plex Discover", extra={"markup": True})
             elif self.config.update_trakt_wl:
-                logger.info(f"Removing {m.title_link} from Trakt watchlist", extra={"markup": True})
+                self.logger.info(f"Removing {m.title_link} from Trakt watchlist", extra={"markup": True})
                 if not dry_run:
                     m.remove_from_trakt_watchlist()
             return
@@ -204,11 +205,11 @@ class Sync:
         if m in self.plex_wl:
             if m not in self.trakt_wl:
                 if self.config.update_trakt_wl:
-                    logger.info(f"Adding {m.title_link} to Trakt watchlist", extra={"markup": True})
+                    self.logger.info(f"Adding {m.title_link} to Trakt watchlist", extra={"markup": True})
                     if not dry_run:
                         m.add_to_trakt_watchlist()
                 else:
-                    logger.info(f"Removing {m.title_link} from Plex watchlist", extra={"markup": True})
+                    self.logger.info(f"Removing {m.title_link} from Plex watchlist", extra={"markup": True})
                     if not dry_run:
                         m.remove_from_plex_watchlist()
             else:
@@ -220,11 +221,11 @@ class Sync:
                 del self.trakt_wl[m]
         elif m in self.trakt_wl:
             if self.config.update_plex_wl:
-                logger.info(f"Adding {m.title_link} to Plex watchlist", extra={"markup": True})
+                self.logger.info(f"Adding {m.title_link} to Plex watchlist", extra={"markup": True})
                 if not dry_run:
                     m.add_to_plex_watchlist()
             else:
-                logger.info(f"Removing {m.title_link} from Trakt watchlist", extra={"markup": True})
+                self.logger.info(f"Removing {m.title_link} from Trakt watchlist", extra={"markup": True})
                 if not dry_run:
                     m.remove_from_trakt_watchlist()
 
@@ -248,6 +249,6 @@ class Sync:
 
         n = len(delete_ids)
         for i, tm in enumerate(delete_items, start=1):
-            logger.info(f"Remove from Trakt collection ({i}/{n}): {tm}")
+            self.logger.info(f"Remove from Trakt collection ({i}/{n}): {tm}")
             if not dry_run:
                 self.trakt.remove_from_collection(tm)
