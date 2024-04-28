2798910467b13a8abe364f50f9e3c11a05a85f63
Elan Ruusamäe <glen@pld-linux.org>
2024-04-28 15:46:40 +0300

    Update LikedListsPlugin to check dry_run not just is_partial

--- plextraktsync/sync/LikedListsPlugin.py
+++ plextraktsync/sync/LikedListsPlugin.py
@@ -28,8 +28,8 @@ class LikedListsPlugin:
         return cls(sync.trakt)
 
     @hookimpl
-    def init(self, trakt_lists: TraktUserListCollection, is_partial: bool):
-        if is_partial:
+    def init(self, trakt_lists: TraktUserListCollection, is_partial: bool, dry_run: bool):
+        if is_partial and not dry_run:
             self.logger.warning("Partial walk, disabling liked lists updating. "
                                 "Liked lists won't update because it needs full library sync.")
         else:
