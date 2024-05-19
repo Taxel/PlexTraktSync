e2915c1108ff534aacb5b607bc78bb4c5b4b8b53
Elan Ruusamäe <glen@pld-linux.org>
2024-05-19 20:27:15 +0300

    Use @decorator in @nocache decorator to remove function nesting

--- plextraktsync/decorators/nocache.py
+++ plextraktsync/decorators/nocache.py
@@ -1,14 +1,11 @@
-from functools import wraps
+from decorator import decorator
 
 from plextraktsync.factory import factory
 
 session = factory.session
 
 
-def nocache(method):
-    @wraps(method)
-    def inner(*args, **kwargs):
-        with session.cache_disabled():
-            return method(*args, **kwargs)
-
-    return inner
+@decorator
+def nocache(method, *args, **kwargs):
+    with session.cache_disabled():
+        return method(*args, **kwargs)
