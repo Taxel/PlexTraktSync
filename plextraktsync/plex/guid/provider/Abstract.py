c35e299b6c5892043d53c2f7842334719117547b
Elan Ruusamäe <glen@pld-linux.org>
2025-01-01 17:54:48 +0200

    Add construct for plex provider abstract

--- plextraktsync/plex/guid/provider/Abstract.py
+++ plextraktsync/plex/guid/provider/Abstract.py
@@ -1,5 +1,11 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from plextraktsync.plex.guid.PlexGuid import PlexGuid
+
 
 class Abstract:
-    pass
+    def __init__(self, guid: PlexGuid):
+        self.guid = guid
