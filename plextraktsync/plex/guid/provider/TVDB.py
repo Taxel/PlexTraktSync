bc72f4c07d26d3943c497f9ec03fed0064d3d85d
Elan Ruusamäe <glen@pld-linux.org>
2025-01-01 18:06:59 +0200

    Update TVDB to extend from Abstract

--- plextraktsync/plex/guid/provider/TVDB.py
+++ plextraktsync/plex/guid/provider/TVDB.py
@@ -1,19 +1,11 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from functools import cached_property
 
-if TYPE_CHECKING:
-    from plextraktsync.plex.guid.PlexGuid import PlexGuid
+from .Abstract import Abstract
 
 
-class TVDB:
-    def __init__(self, guid: PlexGuid):
-        self.guid = guid
-
-    @property
+class TVDB(Abstract):
+    @cached_property
     def link(self):
         return f"https://www.thetvdb.com/dereferrer/{self.guid.type}/{self.guid.id}"
-
-    @property
-    def title(self):
-        return f"{self.guid.provider}:{self.guid.type}:{self.guid.id}"
