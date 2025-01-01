95a3dc09970939df183d8351dbfc5815f3ef4c11
Elan Ruusamäe <glen@pld-linux.org>
2025-01-01 18:05:17 +0200

    Update Local to extend from Abstract

--- plextraktsync/plex/guid/provider/Local.py
+++ plextraktsync/plex/guid/provider/Local.py
@@ -1,19 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from plextraktsync.plex.guid.PlexGuid import PlexGuid
+from .Abstract import Abstract
 
 
-class Local:
-    def __init__(self, guid: PlexGuid):
-        self.guid = guid
-
-    @property
-    def link(self):
-        return None
-
-    @property
-    def title(self):
-        return f"{self.guid.provider}:{self.guid.type}:{self.guid.id}"
+class Local(Abstract):
+    pass
