81345ed349360e92546df5a47d4179cd9d802c42
Elan Ruusamäe <glen@pld-linux.org>
2025-01-02 04:00:49 +0200

    Refactor tmdb link to reuse common url parts

--- plextraktsync/plex/guid/provider/TMDB.py
+++ plextraktsync/plex/guid/provider/TMDB.py
@@ -10,13 +10,22 @@ class TMDB(Abstract):
 
     @cached_property
     def link(self):
-        if self.guid.type == "season":
-            return f"{self.url}/tv/{self.show_guid.id}/season/{self.season_number}"
+        url = f"{self.url}/{self.type}"
+        type = self.guid.type
 
-        if self.guid.type == "episode":
-            return f"{self.url}/tv/{self.show_guid.id}/season/{self.season_number}/episode/{self.episode_number}"
+        if type in ["show", "season", "episode"]:
+            url += f"/{self.show_guid.id}"
 
-        return f"{self.url}/{self.type}/{self.guid.id}"
+        if type in ["season", "episode"]:
+            url += f"/season/{self.season_number}"
+
+        if type == "episode":
+            url += f"/episode/{self.episode_number}"
+
+        if type == "movie":
+            url += f"/{self.guid.id}"
+
+        return url
 
     @property
     def show_guid(self):
