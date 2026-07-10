a8671156a92e883624abf7120c2f1370d09231f4
Elan Ruusamäe <glen@pld-linux.org>
2026-07-10 17:41:22 +0300

    Use trakt.get_personal_list to fetch personal list

--- plextraktsync/trakt/TraktUserList.py
+++ plextraktsync/trakt/TraktUserList.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 
 from trakt.movies import Movie
 from trakt.tv import TVEpisode, TVSeason, TVShow
-from trakt.users import PublicList, User
+from trakt.users import PublicList
 
 from plextraktsync.factory import factory, logging
 from plextraktsync.trakt.types import TraktPlayable
@@ -104,7 +104,7 @@ class TraktUserList:
         username = trakt.me.username
         if self.list_type == "personal" and self.username == username:
             # For user's personal lists, use the user's personal list endpoint
-            user_list = User(username).get_list(self.name)
+            user_list = trakt.get_personal_list(username, self.name)
             self.logger.info(f"Downloaded private personal Trakt list '{user_list.name}' ({len(user_list._items)} items)")
             return user_list.description, self.build_dict_from_raw_items(user_list._items)
         elif not self.is_private:
